dfa9d573773ef9af5923777853489b34f1bed19f
tilly <till.aust@uni-konstanz.de>
2024-07-11 10:59:05 +0200

    make roc plots

--- roc_curve.py
+++ roc_curve.py
@@ -23,20 +23,29 @@ elif platform.system() == "Windows":
 
 
 ## load training/validation data: only for testing "Exp47_Ivy5"
-exp_names = ["Exp44_Ivy2", "Exp45_Ivy4", "Exp46_Ivy0"]
-sensors = ["pn1"]
+exp_names = ["Exp44_Ivy2", "Exp45_Ivy4", "Exp46_Ivy0", "Exp47_Ivy5"]
+sensors = ["pn1"]  # "pn1", "pn3", "mu_ch1", "mu_ch2"
+scoring = "roc_auc"
 
-X, y = load_tsfresh_feature(exp_names, sensors, clean=True)
+X, y = load_tsfresh_feature(exp_names, sensors, split=True)
 
 ## select hyperparameters
 roc_repeats = 500
 number_trees = 1000
 
+setting = pd.read_csv(f"results/naml_history_{scoring}_{sensors}.csv")
+setting.sort_values(by=scoring, inplace=True, ascending=False)
+pipeline = setting.iloc[0]["pipeline"]
 
-learner = ExtraTreesClassifier(n_estimators=number_trees)
+
+data_pre_processor = None
+learner = RandomForestClassifier(criterion='entropy', max_features=7,
+                                        min_samples_leaf=6,
+                                        min_samples_split=14, n_estimators=512,
+                                        warm_start=True)
 pl_interpretable = get_pipeline_for_features(learner, X, y, list(X.columns))
 
 fig, axs = plt.subplots(1, 1, figsize=(7, 7))
 get_mccv_ROC_display(pl_interpretable, X, y, repeats=roc_repeats, ax=axs)
-plt.savefig(f"plots/roc_curve_repeats_{roc_repeats}_extra_tree_{number_trees}_all_features.pdf")
+plt.savefig(f"plots/roc_curve_repeats_{roc_repeats}_{sensors}_all_features.png")
 plt.show()
--- utils/learner_pipeline.py
+++ utils/learner_pipeline.py
@@ -1,11 +1,13 @@
 from sklearn.pipeline import Pipeline
 
 
-def get_pipeline_for_features(classifier, X, y, feature_list):
+# def get_pipeline_for_features(classifier, X, y, feature_list):
+def get_pipeline_for_features(classifier, data_pre_processor, X=None, y=None, feature_list=None):
     steps = []
     # TODO we need no encoding
     # attributes_that_require_encoding = list(set(feature_list) & set(categorical_attributes))
     # if attributes_that_require_encoding:
     #     steps.append(("Categorical Encoder", make_column_transformer((OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1), attributes_that_require_encoding), remainder="passthrough")))
+    steps.append(('data-pre-processor', data_pre_processor))
     steps.append(("Learner", classifier))
     return Pipeline(steps)
